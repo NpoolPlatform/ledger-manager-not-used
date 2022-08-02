293b8a8ee2c8dd0df2dd4d32d57b3ae45e10a442
Zhao KK <kikakkz@hotmail.com>
2022-08-02 04:45:28 +0000

    Add profit cleint

--- api/api.go
+++ api/api.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/NpoolPlatform/ledger-manager/api/detail"
 	"github.com/NpoolPlatform/ledger-manager/api/general"
+	"github.com/NpoolPlatform/ledger-manager/api/profit"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
@@ -20,16 +21,11 @@ func Register(server grpc.ServiceRegistrar) {
 	ledgermgr.RegisterManagerServer(server, &Server{})
 	general.Register(server)
 	detail.Register(server)
+	profit.Register(server)
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
 	if err := ledgermgr.RegisterManagerHandlerFromEndpoint(context.Background(), mux, endpoint, opts); err != nil {
-		return err
-	}
-	if err := general.RegisterGateway(mux, endpoint, opts); err != nil {
-		return err
-	}
-	if err := detail.RegisterGateway(mux, endpoint, opts); err != nil {
 		return err
 	}
 	return nil
